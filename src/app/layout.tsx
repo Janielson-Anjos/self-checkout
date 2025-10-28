0b4d701a542f4e89a62e2f475d2b7c68bd9224c1
Janielso Anjos <jan.tablet.uni@gmail.com>
2025-10-28 18:38:18 -0300

    feat: add initial cart item component(without functionality)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${poppins.className}} antialiased`}>
         <CartProvider>
-        {children}
+          {children}
         </CartProvider>
         </body>
     </html>
